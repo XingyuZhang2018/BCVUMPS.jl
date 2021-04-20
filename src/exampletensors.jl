34f3fb5d79834bafc996a710acc33d4b94d7e70f
XingyuZhang2018 <zhangxingyu@iphy.ac.cn>
2021-04-20 14:41:19 +0800

    differentiable model_tensor

--- src/exampletensors.jl
+++ src/exampletensors.jl
@@ -11,49 +11,41 @@ return the  `MT <: HamiltonianModel` bulktensor at inverse temperature `β` for 
 square lattice tensor-network.
 """
 function model_tensor(model::Ising, β::Real)
-    Ni,Nj = model.Ni, model.Nj
+    Ni, Nj = model.Ni, model.Nj
     ham = [-1. 1;1 -1]
     w = exp.(- β .* ham)
     wsq = sqrt(w)
-    m = ein"ia,ib,ic,id -> abcd"(wsq,wsq,wsq,wsq)
-    M = Array{Array,2}(undef, Ni, Nj)
-
-    for j = 1:Nj, i = 1:Ni
-        M[i,j] = m
-    end
-    return M
+    m = ein"ia,ib,ic,id -> abcd"(wsq, wsq, wsq, wsq)
+    reshape([m for i=1:Ni*Nj], Ni, Nj)
 end
 
 function model_tensor(model::Ising22, β::Real)
     r = model.r
-    wsq, w = Array{Array,1}(undef, 2), Array{Array,1}(undef, 2)
     ham = [-1. 1;1 -1]
-    w[1] = exp.(- β .* ham)
-    w[2] = exp.(- r * β .* ham)
-    wsq = sqrt.(w)
-    M = Array{Array,2}(undef, 2, 2)
-    MT = ein"ia,ib,ic,id -> abcd"(wsq[1],wsq[2],wsq[2],wsq[1])
-    M[1,1] = MT
-    M[1,2] = permutedims(MT,[3,2,1,4])
-    M[2,1] = permutedims(MT,[1,4,3,2])
-    M[2,2] = permutedims(MT,[3,4,1,2])
-    return M
+    w1 = exp.(- β .* ham)
+    w2 = exp.(- r * β .* ham)
+    wsq1 = sqrt(w1)
+    wsq2 = sqrt(w2)
+    M11 = ein"ia,ib,ic,id -> abcd"(wsq1, wsq2, wsq2, wsq1)
+    M12 = permutedims(M11, [3,2,1,4])
+    M21 = permutedims(M11, [1,4,3,2])
+    M22 = permutedims(M11, [3,4,1,2])
+    reshape([M11,M21,M12,M22], 2, 2)
 end
 
+########## To do: correct Ising33 ##########
 function model_tensor(model::Ising33, β::Real)
     r = model.r
-    wsq, w = Array{Array,1}(undef, 2), Array{Array,1}(undef, 2)
     ham = [-1. 1;1 -1]
-    w[1] = exp.(- β .* ham)
-    w[2] = exp.(- r * β .* ham)
-    wsq = sqrt.(w)
-    M = Array{Array,2}(undef, 3, 3)
-    MT = ein"ia,ib,ic,id -> abcd"(wsq[1],wsq[2],wsq[2],wsq[1])
-    M[1,1] = MT
-    M[1,2] = permutedims(MT,[3,2,1,4])
-    M[2,1] = permutedims(MT,[1,4,3,2])
-    M[2,2] = permutedims(MT,[3,4,1,2])
-    return M
+    w1 = exp.(- β .* ham)
+    w2 = exp.(- r * β .* ham)
+    wsq1 = sqrt(w1)
+    wsq2 = sqrt(w2)
+    M11 = ein"ia,ib,ic,id -> abcd"(wsq1, wsq2, wsq2, wsq1)
+    M12 = permutedims(M11, [3,2,1,4])
+    M21 = permutedims(M11, [1,4,3,2])
+    M22 = permutedims(M11, [3,4,1,2])
+    reshape([M11,M21,M12,M22], 2, 2)
 end
 
 """
@@ -68,28 +60,22 @@ function mag_tensor(model::Ising, β::Real)
     cβ, sβ = sqrt(cosh(β)), sqrt(sinh(β))
     q = 1/sqrt(2) * [cβ+sβ cβ-sβ; cβ-sβ cβ+sβ]
     m = ein"abcd,ai,bj,ck,dl -> ijkl"(a,q,q,q,q)
-    M = Array{Array,2}(undef, Ni, Nj)
-    for j = 1:Nj, i = 1:Ni
-        M[i,j] = m
-    end
-    return M
+    reshape([m for i=1:Ni*Nj], Ni, Nj)
 end
 
 function mag_tensor(model::Ising22, β::Real)
     r = model.r
     a = reshape(Float64[1 0 0 0; 0 0 0 0; 0 0 0 0; 0 0 0 -1] , 2,2,2,2)
-    wsq, w = Array{Array,1}(undef, 2), Array{Array,1}(undef, 2)
     ham = [-1. 1;1 -1]
-    w[1] = exp.(- β .* ham)
-    w[2] = exp.(- r * β .* ham)
-    wsq = sqrt.(w)
-    M = Array{Array,2}(undef, 2, 2)
-    MT = ein"ijkl,ia,jb,kc,ld -> abcd"(a,wsq[1],wsq[2],wsq[2],wsq[1])
-    M[1,1] = MT
-    M[1,2] = permutedims(MT,[3,2,1,4])
-    M[2,1] = permutedims(MT,[1,4,3,2])
-    M[2,2] = permutedims(MT,[3,4,1,2])
-    return M
+    w1 = exp.(- β .* ham)
+    w2 = exp.(- r * β .* ham)
+    wsq1 = sqrt(w1)
+    wsq2 = sqrt(w2)
+    M11 = ein"ijkl,ia,jb,kc,ld -> abcd"(a,wsq1,wsq2,wsq2,wsq1)
+    M12 = permutedims(M11, [3,2,1,4])
+    M21 = permutedims(M11, [1,4,3,2])
+    M22 = permutedims(M11, [3,4,1,2])
+    reshape([M11,M21,M12,M22], 2, 2)
 end
 
 """
@@ -107,33 +93,29 @@ function energy_tensor(model::Ising, β::Real)
     wsqi = wsq^(-1)
     e = (ein"ai,im,bm,cm,dm -> abcd"(wsqi,we,wsq,wsq,wsq) + ein"am,bi,im,cm,dm -> abcd"(wsq,wsqi,we,wsq,wsq) + 
         ein"am,bm,ci,im,dm -> abcd"(wsq,wsq,wsqi,we,wsq) + ein"am,bm,cm,di,im -> abcd"(wsq,wsq,wsq,wsqi,we)) ./ 2
-    E = Array{Array,2}(undef, Ni, Nj)
-    for j = 1:Nj, i = 1:Ni
-        E[i,j] = e
-    end
-    return E
+    reshape([e for i=1:Ni*Nj], Ni, Nj)
 end
 
 function energy_tensor(model::Ising22, β::Real)
     r = model.r
-    wsq, w, we = Array{Array,1}(undef, 2), Array{Array,1}(undef, 2), Array{Array,1}(undef, 2)
     ham = [-1. 1;1 -1]
-    w[1] = exp.(- β .* ham)
-    w[2] = exp.(- r * β .* ham)
-    we[1] = ham .* w[1]
-    we[2] = r .* ham .* w[2]
-    wsq = sqrt.(w)
-    wsqi = wsq.^(-1)
+    w1 = exp.(- β .* ham)
+    w2 = exp.(- r * β .* ham)
+    we1 = ham .* w1
+    we2 = r .* ham .* w2
+    wsq1 = sqrt(w1)
+    wsq2 = sqrt(w2)
+    wsqi1 = wsq1^(-1)
+    wsqi2 = wsq2^(-1)
     
     M = Array{Array,2}(undef, 2, 2)
-    MT = (ein"ai,im,bm,cm,dm -> abcd"(wsqi[1],we[1],wsq[2],wsq[2],wsq[1]) + 
-              ein"am,bi,im,cm,dm -> abcd"(wsq[1],wsqi[2],we[2],wsq[2],wsq[1]) + 
-              ein"am,bm,ci,im,dm -> abcd"(wsq[1],wsq[2],wsqi[2],we[2],wsq[1]) + 
-              ein"am,bm,cm,di,im -> abcd"(wsq[1],wsq[2],wsq[2],wsqi[1],we[1])) ./ 2
-    M[1,1] = MT
-    M[1,2] = permutedims(MT,[3,2,1,4])
-    M[2,1] = permutedims(MT,[1,4,3,2])
-    M[2,2] = permutedims(MT,[3,4,1,2])
-    return M
+    M11 = (ein"ai,im,bm,cm,dm -> abcd"(wsqi1,we1,wsq2,wsq2,wsq1) + 
+              ein"am,bi,im,cm,dm -> abcd"(wsq1,wsqi2,we2,wsq2,wsq1) + 
+              ein"am,bm,ci,im,dm -> abcd"(wsq1,wsq2,wsqi2,we2,wsq1) + 
+              ein"am,bm,cm,di,im -> abcd"(wsq1,wsq2,wsq2,wsqi1,we1)) ./ 2
+    M12 = permutedims(M11, [3,2,1,4])
+    M21 = permutedims(M11, [1,4,3,2])
+    M22 = permutedims(M11, [3,4,1,2])
+    reshape([M11,M21,M12,M22], 2, 2)
 end
 
